a3612be1a97fb17afa90329fe9dc7d6624f4f0e3
harupyaaaan <harukasama1225@outlook.jp>
2021-03-08 05:35:26 +0000

    [Finish]課題６

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
 
   get 'home/about' => 'homes#about'
   resources :books do
-  resource :favorites, only: [:create, :destroy]
-  resources :book_comments, only: [:create, :destroy]
+   resource :favorites, only: [:create, :destroy]
+   resources :book_comments, only: [:create, :destroy]
 end
 
 end
